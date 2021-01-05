7afb36f1cdeabd339af7112f9b617c1f40ff7f2d
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-01-05 15:16:32 +0100

    Modif

--- app.js
+++ app.js
@@ -2,18 +2,18 @@
 // 1 jour = 86400000 millisecondes.
 
 // Comment créer un cookie, je suis gentil je laisse l'exemple !
-document.cookie = "theme=dark; path=/; domain=localhost; max-age=86400000; samesite=strict";
-console.log(document.cookie);
+//document.cookie = "theme=dark; path=/; domain=localhost; max-age=86400000; samesite=strict";
+//console.log(document.cookie);
 
 
 /**
  * Récupère tous les cookies.
  * @returns {string[]}
  */
-function getCookies() {
-    let cookies = document.cookie.split(';');
-    return cookies.map(cookie => cookie.trim());
-}
+//function getCookies() {
+  //  let cookies = document.cookie.split(';');
+   // return cookies.map(cookie => cookie.trim());
+//}
 
 
 /**
@@ -24,6 +24,8 @@ function getCookies() {
 function setCookie(cookieName, cookieValue) {
     // Le cookie doit être valide 2 jours et doit respecter les normes de sécurité contre les failles CSRF.
     // Votre code ici.
+    document.cookie = cookieName + "=" + cookieValue + ";path=/; domain=localhost; max-age=172800000; samesite=strict";
+
 }
 
 
@@ -33,14 +35,16 @@ function setCookie(cookieName, cookieValue) {
  */
 function getCookie(cookieName) {
     // Votre code ici.
+    let cookies = document.cookie.split(';');
+    return cookies.map(cookieName => cookieName.trim());
 }
 
 
-console.log(getCookies());
+console.log(getCookie());
 
 // FIXME Test de setCookie()
 setCookie('monCookie', 'maValeur');
-console.log(getCookies());
+console.log(getCookie());
 
 // FIXME test de getCookie(param)
-console.log(getCookies('monCookie'));
+console.log(getCookie('monCookie'));
